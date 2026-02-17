0b23e63a597c9b9833d53ae8e335a9e43d9dc4fe
zakarya hari <zakariahari42@gmail.com>
2026-02-17 23:58:12 +0000

    feat: création de la migration rooms avec number, type, price et status

---
+++ database/migrations/2026_02_17_214810_create_rooms_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->string('number')->unique();
+            $table->string('type');
+            $table->decimal('price', 8, 2);
+            $table->enum('status', ['available', 'occupied', 'maintenance'])->default('available');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rooms');
+    }
+};
