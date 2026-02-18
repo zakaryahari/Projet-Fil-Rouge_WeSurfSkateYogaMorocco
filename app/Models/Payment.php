a1397c36940a778a0220833fef6fa0414612afbd
zakarya hari <zakariahari42@gmail.com>
2026-02-18 16:39:49 +0000

    feat: création du modèle Payment avec la relation booking

---
+++ app/Models/Payment.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Booking;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    /** @use HasFactory<\Database\Factories\PaymentFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'booking_id',
+        'amount',
+        'paymentDate',
+        'method',
+        'status',
+    ];
+
+    public function booking()
+    {
+        return $this->belongsTo(Booking::class);
+    }
+}
