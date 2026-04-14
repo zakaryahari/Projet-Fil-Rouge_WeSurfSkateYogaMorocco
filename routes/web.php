ea4ef3d43171505d0beb5dcc6897f2557f9c2131
zakarya hari <zakariahari42@gmail.com>
2026-04-14 05:54:13 +0100

    feat: ajouter routes pour pages statiques

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,11 @@ Route::get('/', function () {
 // Public Package Details Route
 Route::get('/packages/{id}', [\App\Http\Controllers\PublicPackageController::class, 'show'])->name('packages.show');
 
+// Public Static Pages Routes
+Route::view('/events', 'pages.events')->name('events');
+Route::view('/about', 'pages.about')->name('about');
+Route::view('/contact', 'pages.contact')->name('contact');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
