0de2bef4132c9e09ba5e57d1a0a93468723ef362
zakarya hari <zakariahari42@gmail.com>
2026-04-16 11:18:39 +0100

    fix: renommer le champ phone vers phone_number dans la factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,7 +26,7 @@ class UserFactory extends Factory
         return [
             'name'               => fake()->name(),
             'email'              => fake()->unique()->safeEmail(),
-            'phone'              => fake()->unique()->numerify('+2126########'),
+            'phone_number'       => fake()->unique()->numerify('+2126########'),
             'email_verified_at'  => now(),
             'password'           => static::$password ??= Hash::make('password'),
             'role'               => 'customer',
