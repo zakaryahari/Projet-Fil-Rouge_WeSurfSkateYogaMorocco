aaa79ea2f73fb53f30200471816f7e0f2bdff3ab
zakarya hari <zakariahari42@gmail.com>
2026-04-23 12:25:56 +0100

    refactor: remove 'name' field from fillable attributes in Room model

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -10,7 +10,7 @@ class Room extends Model
     /** @use HasFactory<\Database\Factories\RoomFactory> */
     use HasFactory;
 
-    protected $fillable = ['name', 'type', 'capacity', 'price_per_night', 'total_stock', 'is_active', 'image_path'];
+    protected $fillable = ['type', 'capacity', 'price_per_night', 'total_stock', 'is_active', 'image_path'];
 
     
     public function bookings()
