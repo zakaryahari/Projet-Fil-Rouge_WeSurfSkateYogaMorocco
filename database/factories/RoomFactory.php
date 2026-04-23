baae961c4662a9da1520a4d60df35a7ae3611e7c
zakarya hari <zakariahari42@gmail.com>
2026-04-23 12:26:04 +0100

    refactor: update RoomFactory to standardize room types and correct image path

--- database/factories/RoomFactory.php
+++ database/factories/RoomFactory.php
@@ -9,13 +9,12 @@ class RoomFactory extends Factory
     public function definition(): array
     {
         return [
-            'name'            => $this->faker->words(2, true) . ' Room',
-            'type'            => $this->faker->randomElement(['single', 'double', 'suite', 'dorm']),
+            'type'            => $this->faker->randomElement(['Standard', 'Deluxe', 'Suite', 'Villa', 'Beach House']),
             'capacity'        => $this->faker->numberBetween(1, 6),
             'price_per_night' => $this->faker->randomFloat(2, 30, 300),
             'total_stock'     => $this->faker->numberBetween(1, 10),
             'is_active'       => true,
-            'image_path'      => 'images/rooms/' . $this->faker->slug(2) . '.jpg',
+            'image_path'      => 'rooms/' . $this->faker->slug(2) . '.jpg',
         ];
     }
 }
